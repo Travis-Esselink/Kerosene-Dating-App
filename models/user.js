6bd65a8240893744b64007cbd5159dcdb0588c0e
christopherhendricksonga <christopher.hendricksonacc@gmail.com>
2022-10-28 19:52:57 +1300

    scaff

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const passportLocalMongoose = require('passport-local-mongoose')
+const Schema = mongoose.Schema
+
+const matchSchema = new Schema({
+    match: [{type:Schema.Types.ObjectId,  ref: "User"}],
+    chatRoomID: {type:String, required:true}
+})
+
+const seenSchema = new Schema({
+    user: {type:Schema.Types.ObjectId,  ref: "User"},
+    liked: Boolean
+})
+
+const userSchema = new Schema({
+    displayName:{type:String},
+    dateOfBirth:{type:Date},
+    gender:{type:String},
+    genderPref:{type:String},
+    agePref:{type:Number},
+    
+    bio:{type:String},
+    coverImage:{type:String},
+    images:{type:String},
+    matches: [{type:matchSchema}],
+    seen: [{type:seenSchema}]
+}, 
+{ timestamps: true }
+) 
+
+
+
+userSchema.plugin(passportLocalMongoose)
+
+const User = mongoose.model('User',userSchema)
+
+
+module.exports = User

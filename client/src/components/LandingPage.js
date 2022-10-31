b38e493580d640046b7bdafacc010980774f9340
Chin Ying Kong <chinYingAdora>
2022-10-31 15:47:58 +1100

    Only created CreateAccount & Login components

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -9,8 +9,17 @@ const LandingPage = () => {
                 <h1>Find The Right One, Right Here, Right Now</h1>
                 <button className="createAcc-button">Create Account</button>
             </div>
+
+            { }
+            
         </div>
     )
 }
 
 export default LandingPage
+
+// show the modal for register or login
+// Assume all users are not our users-> SignUp: true. once click on login button, setSignUp to false 
+//  - if isSignUp is true, then bring up the register form.
+//  - if isSignUp is false, then bring up the login form
+// Either register or login, once clicked, bring up the form modal. modal: true or false?

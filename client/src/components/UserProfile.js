1f2482b418bc4b5d7c29e95bffb9b636621618bd
Chin Ying Kong <chinYingAdora>
2022-11-03 11:45:44 +1100

    Added 2 functions to convert date and calculate age

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -7,6 +7,17 @@ import Card from 'react-bootstrap/Card';
 import MatchNav from "./MatchNav"
 import Loading from "./Loading"
 
+const formatDate = (dateStr) => {
+    return new Date(dateStr).toISOString().substring(0,10)
+}
+
+const calculateAge = (dateStr) => {
+    const date = new Date(dateStr).toISOString().substring(0,10)
+    const ageDifMs = Date.now() - new Date(date).getTime();
+    const ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
 const UserProfile = ({user}) => {
     const [userData, setUserData] = useState({})
     const { id } = useParams()
@@ -36,7 +47,8 @@ const UserProfile = ({user}) => {
                 <h5>{userData.displayName}</h5>
                 <ul>
                     <li>Preferred Name: {userData.displayName}</li>
-                    <li>Date of Birth{userData.dateOfBirth}</li>
+                    <li>Age: {calculateAge(userData.dateOfBirth)}</li>
+                    <li>Date of Birth: {formatDate(userData.dateOfBirth)}</li>
                     <li>Gender: {userData.gender}</li>
                     <li>Interested In: {userData.genderPref}</li>
                     <li>About Me: {userData.bio}</li>

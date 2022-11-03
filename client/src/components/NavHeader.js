b530dede898b4c6a25b1b024688477ec1e5f59c6
Chin Ying Kong <chinYingAdora>
2022-11-03 22:40:57 +1100

    Added conditional render for login button

--- client/src/components/NavHeader.js
+++ client/src/components/NavHeader.js
@@ -6,12 +6,11 @@ import ThemeProvider from 'react-bootstrap/ThemeProvider';
 
 import Login from "./Login"
 
-const NavHeader = ({user, setUser, setIsSignUp}) => {
+const NavHeader = ({user, setUser}) => {
     const [modalShow, setModalShow] = useState(false);
 
     const handleClick = () => {
         setModalShow(true)
-        setIsSignUp(false)
     }
 
     return (
@@ -21,9 +20,11 @@ const NavHeader = ({user, setUser, setIsSignUp}) => {
                 <Link to="/"><img className="logo" src={AppLogo} alt="App Logo" /></Link>
             </div>
 
-            <ThemeProvider prefixes={{ btn: 'login-button' }}>
-            <Button onClick={handleClick}>Login</Button>
-            </ThemeProvider>
+            { user &&
+                <ThemeProvider prefixes={{ btn: 'login-button' }}>
+                <Button onClick={handleClick}>Login</Button>
+                </ThemeProvider>
+            }
 
         </nav>
         <Login setUser={setUser} show={modalShow} onHide={() => setModalShow(false)} />

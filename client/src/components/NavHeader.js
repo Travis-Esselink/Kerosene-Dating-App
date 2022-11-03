9b3acf6ed3844b7fe2261456ddaf4e879ef7e3f7
Chin Ying Kong <chinYingAdora>
2022-11-03 20:45:06 +1100

    Removed Logout button

--- client/src/components/NavHeader.js
+++ client/src/components/NavHeader.js
@@ -5,7 +5,6 @@ import Button from 'react-bootstrap/Button';
 import ThemeProvider from 'react-bootstrap/ThemeProvider';
 
 import Login from "./Login"
-import LogoutButton from "./LogoutButton";
 
 const NavHeader = ({user, setUser, setIsSignUp}) => {
     const [modalShow, setModalShow] = useState(false);
@@ -25,7 +24,6 @@ const NavHeader = ({user, setUser, setIsSignUp}) => {
             <ThemeProvider prefixes={{ btn: 'login-button' }}>
             <Button onClick={handleClick}>Login</Button>
             </ThemeProvider>
-            <LogoutButton setUser={setUser} />
 
         </nav>
         <Login setUser={setUser} show={modalShow} onHide={() => setModalShow(false)} />
